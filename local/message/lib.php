3492e2acf60962bfd86699c36f68d2e876cae63e
Gerald Scott <geraldo@archton.io>
2024-05-29 12:26:04 +0200

    Message plugin

--- local/message/lib.php
+++ local/message/lib.php
@@ -24,5 +24,26 @@ use local_message\manager;
 function local_message_before_footer() {
     global $USER;
 
-    \core\notification::add('Well hello!', \core\output\notification::NOTIFY_SUCCESS);
+    if (!get_config('local_message', 'enabled')) {
+        return;
+    }
+
+    $manager = new manager();
+    $messages = $manager->get_messages($USER->id);
+
+    foreach ($messages as $message) {
+        $type = \core\output\notification::NOTIFY_INFO;
+        if ($message->messagetype === '0') {
+            $type = \core\output\notification::NOTIFY_WARNING;
+        }
+        if ($message->messagetype === '1') {
+            $type = \core\output\notification::NOTIFY_SUCCESS;
+        }
+        if ($message->messagetype === '2') {
+            $type = \core\output\notification::NOTIFY_ERROR;
+        }
+        \core\notification::add($message->messagetext, $type);
+
+        $manager->mark_message_read($message->id, $USER->id);
+    }
 }
