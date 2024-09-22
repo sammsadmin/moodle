25c2a0aedabed4df686d28cfe967381e0745766b
Gerald Scott <gerald@archton.io>
2024-09-22 07:14:53 +0200

    Add placeholder test

--- blocks/learning_log/classes/forms/add_form.php
+++ blocks/learning_log/classes/forms/add_form.php
@@ -7,18 +7,18 @@ class block_learning_log_add_form extends moodleform {
     public function definition() {
         $mform = $this->_form;
 
-        $mform->addElement('date_selector', 'event_date_int', get_string('event_date', 'block_learning_log'));
-        $mform->addRule('event_date_int', null, 'required', null, 'client');
-
-        $mform->addElement('text', 'description', get_string('description', 'block_learning_log'));
+        $mform->addElement('text', 'description', get_string('description', 'block_learning_log'),['placeholder' => get_string('description_ph', 'block_learning_log')]);
         $mform->setType('description', PARAM_TEXT);
         $mform->addRule('description', null, 'required', null, 'client');
 
-        $mform->addElement('text', 'organisation', get_string('organisation', 'block_learning_log'));
+        $mform->addElement('text', 'organisation', get_string('organisation', 'block_learning_log'),['placeholder' => get_string('organisation_ph', 'block_learning_log')]);
         $mform->setType('organisation', PARAM_TEXT);
         $mform->addRule('organisation', null, 'required', null, 'client');
 
-        $mform->addElement('text', 'duration', get_string('duration', 'block_learning_log'));
+        $mform->addElement('date_selector', 'event_date_int', get_string('event_date', 'block_learning_log'));
+        $mform->addRule('event_date_int', null, 'required', null, 'client');
+
+        $mform->addElement('text', 'duration', get_string('duration', 'block_learning_log'),['placeholder' => get_string('duration_ph', 'block_learning_log')]);
         $mform->setType('duration', PARAM_FLOAT);
         $mform->addRule('duration', null, 'required', null, 'client');
         $mform->addRule('duration', 'Enter number of hours (e.g. 1 or 2.25)', 'numeric', null, 'client');
